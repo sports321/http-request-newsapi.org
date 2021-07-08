aecdacb6560fb24235ba38d1154a3783912ad2f2
sports321 <sicilyvonclaude321@gmail.com>
2021-07-08 16:10:06 +0700

    selesai mengerjakan

---
+++ bin/newsapi.dart
@@ -0,0 +1,5 @@
+import 'package:newsapi/newsapi.dart' as newsapi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${newsapi.calculate()}!');
+}
---
+++ test/newsapi_test.dart
@@ -0,0 +1,8 @@
+import 'package:newsapi/newsapi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
